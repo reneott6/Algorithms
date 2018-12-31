21fa8f41c945d9b01681d40a28c187035dc57e93
Rene Ott <rene.ott6@gmail.com>
2018-12-31 23:18:05 +0200

    Misc

---
+++ Algorithms/Misc/LinearSearch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Misc
+{
+    public class LinearSearch
+    {
+        private readonly IList<int> list;
+        private readonly int value;
+
+        public LinearSearch(IList<int> list, int value)
+        {
+            this.value = value;
+            this.list = list;
+        }
+
+        public int? FindValue()
+        {
+            // Invariant loop: value doesn't exist in list[k..i-1], where's k integer & k <= i
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] == value)
+                    return i;
+            }
+
+            return null;
+        }
+    }
+}
